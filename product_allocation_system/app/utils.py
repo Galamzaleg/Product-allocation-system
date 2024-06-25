aa7b5fd0d6bad4c42bd2679ced29821d2cfcb517
Galamzaleg <gal@profitzon.net>
2024-06-25 14:39:24 +0300

    Create utils.py

---
+++ product_allocation_system/app/utils.py
@@ -0,0 +1,8 @@
+from .models import db, ASIN, Client, Store, UngatingStatus, Preorder, Pallet
+import datetime
+import uuid
+
+def generate_pallets():
+    max_volume = 138240  # cubic inches
+    max_weight = 2200  # lbs
+    full_truck |oai:code-citation|
